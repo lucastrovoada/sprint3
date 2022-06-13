6a359e26f4b0e683c6d878f2667ba09d0d2f2072
lucastrovoada <106043567+lucastrovoada@users.noreply.github.com>
2022-06-12 22:51:50 -0300

    primeiro edit

---
+++ script.js
@@ -0,0 +1,81 @@
+let filterA = document.getElementById("blur");
+let filterB = document.getElementById("contrast");
+let filterC = document.getElementById("hue-rotate");
+let filterD = document.getElementById("sepia");
+
+let noFlipBtn = document.getElementById("no-flip");
+let flipXBtn = document.getElementById("flip-x");
+let flipYBtn = document.getElementById("flip-y");
+
+let uploadButton = document.getElementById("upload-button");
+let image = document.getElementById("chosen-image");
+
+
+function resetFilter(){
+    filterA.value = "0";
+    filterB.value = "100";
+    filterC.value = "0";
+    filterD.value = "0";
+    noFlipBtn.checked = true;
+    addFilter();
+    flipImage();
+}
+
+uploadButton.onchange = () => {
+    resetFilter();
+    document.querySelector(".image-container").style.display = "block";
+    let reader = new FileReader();
+    reader.readAsDataURL(uploadButton.files[0]);
+    reader.onload = () => {
+        image.setAttribute("src", reader.result);
+    }
+}
+
+let sliders = document.querySelectorAll(".filter input[type='range']");
+sliders.forEach( slider => {
+    slider.addEventListener("input", addFilter);
+});
+
+function addFilter(){
+    image.style.filter = `blur(${filterA.value}px) contrast(${filterB.value}%) hue-rotate(${filterC.value}deg) sepia(${filterD.value}%)`;
+}
+
+let radioBtns = document.querySelectorAll(".flip-option input[type='radio']");
+radioBtns.forEach( radioBtn => {
+    radioBtn.addEventListener("click", flipImage);
+});
+
+function flipImage(){
+    if(flipXBtn.checked){
+        image.style.transform = "scaleX(-1)";
+    }
+    else if(flipYBtn.checked){
+        image.style.transform = "scaleY(-1)";
+    }
+    else{
+        image.style.transform = "scale(1,1)";
+    }
+}
+
+function cropImg(){
+    const canvas = document.getElementById('image');
+    const ctx = canvas.getContext('2d');
+  
+    var image = new Image();
+    
+    image.onload = function(){
+      ctx.drawImage(image, 150, 200, 500, 300, 60,60, 500, 300);
+    }
+  }
+  
+  cropImg();
+
+  const downloadBtn = document.querySelector("button.download");
+ 
+downloadBtn.addEventListener('click', function() {
+    let tempLink = document.createElement('a');
+    tempLink.download = 'puppy-cropped.jpg';
+    tempLink.href = document.getElementById('canvas').toDataURL("image/jpeg", 0.9);
+    tempLink.click();
+});
+
